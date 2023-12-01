c66d8ef706ef033edd2e2f13c1f891be6d71a4fb
Gautham Prem Krishnan <gauthampremkrishnan@Gauthams-MBP.lan>
2023-12-01 16:55:22 -0500

    Fix port

--- app.js
+++ app.js
@@ -101,8 +101,8 @@ app.post('/generate-pdf', (req, res) => {
     doc.end();
 });
 
+const PORT = process.env.PORT || 8000; 
 
-
-app.listen(8000, () => {
-    console.log(`Server is running on port 8000.`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
 });
